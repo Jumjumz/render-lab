5b962c7b78a31327a08e5977ddbcd25040617b48
Jummer Ramos <jumjumramos@gmail.com>
2025-12-24 05:23:40 +0800

    final format for clang

--- image.cpp
+++ image.cpp
@@ -4,23 +4,23 @@
 #include <ostream>
 
 int main() {
-  int image_width = 256;
-  int image_height = 256;
+    int image_width = 256;
+    int image_height = 256;
 
-  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-  for (int j = 0; j < image_height; j++) {
-    std::clog << "\nscanlines remaining: " << (image_height - j) << ' '
-              << std::flush;
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\nscanlines remaining: " << (image_height - j) << ' '
+                  << std::flush;
 
-    for (int i = 0; i < image_width; i++) {
-      auto pixel_color = color(double(i) / (image_width - 1),
-                               double(j) / (image_height - 1), 0);
-      write_color(std::cout, pixel_color);
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_color = color(double(i) / (image_width - 1),
+                                     double(j) / (image_height - 1), 0);
+            write_color(std::cout, pixel_color);
+        }
     }
-  }
 
-  std::clog << "\nDone.\n";
+    std::clog << "\nDone.\n";
 
-  return 0;
+    return 0;
 }
