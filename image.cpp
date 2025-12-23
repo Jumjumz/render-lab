9a0db20064ece384b82068b0e98d594ab6d9fa89
Jummer Ramos <jumjumramos@gmail.com>
2025-12-24 03:59:04 +0800

    updated format

--- image.cpp
+++ image.cpp
@@ -10,9 +10,11 @@ int main() {
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
   for (int j = 0; j < image_height; j++) {
-    std::clog << "\nScanLines remaining: " << (image_height - j) << ' ' << std::flush;
+    std::clog << "\nscanlines remaining: " << (image_height - j) << ' ' << std::flush;
+    
     for (int i = 0; i < image_width; i++) {
-      auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+      auto pixel_color = color(double(i) / (image_width - 1),
+                               double(j) / (image_height - 1), 0);
       write_color(std::cout, pixel_color);
     }
   }
