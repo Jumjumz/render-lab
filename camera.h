b7af3c61e5c939f45d94265fdccd1385fc75568a
Jummer Ramos <jumjumramos@gmail.com>
2025-12-27 00:59:27 +0800

    updated camera.h to use inline variables and function

--- camera.h
+++ camera.h
@@ -6,8 +6,7 @@
 using camera = point3;
 
 // Can add a camera class for camera manipulation
-
-inline const camera camera_center() { return camera(0, 0, 0); };
+inline const camera camera_center = camera(0, 0, 0);
 
 inline camera camera_location(double x, double y, double focal_point) {
     return camera(x, y, focal_point);
