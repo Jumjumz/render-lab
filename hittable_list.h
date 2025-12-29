d7eaa16cfcc7d84e5472ff0e8acdb0a58d72f519
Jummer Ramos <jumjumramos@gmail.com>
2025-12-29 21:58:19 +0800

    updated to use std

--- hittable_list.h
+++ hittable_list.h
@@ -7,14 +7,14 @@
 
 class hittable_list : public hittable {
   public:
-    std::vector<shared_ptr<hittable>> objects;
+    std::vector<std::shared_ptr<hittable>> objects;
 
     hittable_list() {}
-    hittable_list(shared_ptr<hittable> object) { add(object); };
+    hittable_list(std::shared_ptr<hittable> object) { add(object); };
 
     void clear() { objects.clear(); };
 
-    void add(shared_ptr<hittable> object) { objects.push_back(object); }
+    void add(std::shared_ptr<hittable> object) { objects.push_back(object); }
 
     bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
         hit_record temp_rec;
