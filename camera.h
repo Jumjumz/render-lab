ed268c1c7f1071b5ce6c252afc8b5e591f03566c
Jummer Ramos <jumjumramos@gmail.com>
2026-01-01 00:28:33 +0800

    updated camera

--- camera.h
+++ camera.h
@@ -9,7 +9,11 @@ class camera {
     int image_width = 100;
     int samples_per_pixel = 10;
     int max_depth = 10; // maximun number of ray bounces into scene
-    double vfov = 90;   // vertiacal view angle (FOV)
+
+    double vfov = 90; // vertiacal view angle (FOV)
+    point3 look_from = point3(0, 0, 0);
+    point3 look_at = point3(0, 0, -1);
+    vec3 vup = vec3(0, 1, 0);
 
     void render(const hittable &world) {
         initialize();
@@ -40,6 +44,7 @@ class camera {
     point3 pixel00_loc;
     vec3 pixel_delta_u;
     vec3 pixel_delta_v;
+    vec3 u, v, w; // camera frame base vectors
 
     void initialize() {
         image_height = int(image_width / aspect_ratio);
@@ -47,19 +52,24 @@ class camera {
 
         pixel_samples_scale = 1.0 / samples_per_pixel;
 
-        center = point3(0, 0, 0);
+        center = look_from;
 
         // Determine viewport dimenstions
-        auto focal_length = 1.0;
+        auto focal_length = (look_from - look_at).length();
         auto theta = degrees_to_radians(vfov);
         auto h = std::tan(theta / 2);
         auto viewport_height = 2.0 * h * focal_length;
         auto viewport_width =
             viewport_height * (double(image_width) / image_height);
 
+        // Calculate the u, v, w unit base vectors for the camera coordinate frame
+        w = unit_vector(look_from - look_at);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
+
         // Calculate vectors across the horizontal and vertical down to the viewport edge
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0);
+        auto viewport_u = viewport_width * u;
+        auto viewport_v = viewport_height * -v;
 
         // Calculate the horizontal and vertical delta vectors from pixel to pixel
         pixel_delta_u = viewport_u / image_width;
@@ -67,7 +77,7 @@ class camera {
 
         // Calculate the location of the upper left pixel
         auto viewport_upper_left =
-            center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+            center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
         pixel00_loc =
             viewport_upper_left +
             0.5 * (pixel_delta_u +
