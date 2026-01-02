4342ebfee63542f8bab3d0ca9b1f087c6c120ba3
Jummer Ramos <jumjumramos@gmail.com>
2026-01-03 00:45:03 +0800

    add sdl renderer

--- render.cpp
+++ render.cpp
@@ -1,28 +1,34 @@
 #include <SDL2/SDL.h>
-#include <iostream>
 
 int main(int argc, char *argv[]) {
     int window_width = 800;
     int window_height = 600;
 
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
-
-        return 1;
-    }
-
+    SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *window = SDL_CreateWindow("Renderer", SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED, window_width,
                                           window_height, SDL_WINDOW_SHOWN);
 
-    if (!window) {
-        std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
+    SDL_Renderer *renderer =
+        SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-        return 1;
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
     }
 
-    SDL_Delay(10000);
-
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
