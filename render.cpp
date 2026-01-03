d9afd843f634cceff581063448bbb5ce4269b3a5
Jummer Ramos <jumjumramos@gmail.com>
2026-01-03 08:19:27 +0800

    render rectangle

--- render.cpp
+++ render.cpp
@@ -1,4 +1,8 @@
+#include "SDL_rect.h"
+#include "SDL_render.h"
 #include <SDL2/SDL.h>
+
+SDL_Rect rectangle = {0, 0, 300, 120};
 
 int main(int argc, char *argv[]) {
     int window_width = 1280;
@@ -6,24 +10,22 @@ int main(int argc, char *argv[]) {
 
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *window = SDL_CreateWindow(
-        "Wireframe renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        "Wireframe Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         window_width, window_height, SDL_WINDOW_SHOWN);
 
     SDL_Renderer *renderer =
         SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    bool running = true;
     SDL_Event event;
+    bool running = true;
 
     while (running) {
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+            if (event.type == SDL_QUIT)
                 running = false;
-            }
         }
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+        SDL_RenderFillRect(renderer, &rectangle);
 
         SDL_RenderPresent(renderer);
     }
