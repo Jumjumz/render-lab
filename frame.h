f3f1453156cc5e7096e08f1b7845a6f589bbe953
Jummer Ramos <jumjumramos@gmail.com>
2026-01-05 09:00:01 +0800

    added frame.h

---
+++ frame.h
@@ -0,0 +1,20 @@
+#ifndef FRAME_H
+#define FRAME_H
+
+#include "SDL_rect.h"
+#include "screen.h"
+
+inline SDL_Rect frame(double delta_time, int &window_width, int &window_height,
+                      int &rec_size) {
+    int dz = 0;
+    dz += 1 * delta_time;
+
+    screen s(0.5, 0.5, 1 + dz);
+    screen p = project(s.x, s.y, s.z);
+    screen d = display(p.x, p.y, window_width, window_height);
+
+    return SDL_Rect{offset(d.x, rec_size), offset(d.y, rec_size), rec_size,
+                    rec_size};
+}
+
+#endif // !FRAME_H
