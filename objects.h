be00e21852b5bd2114f5f9019904689d941bb164
Jummer Ramos <jumjumramos@gmail.com>
2026-01-10 04:29:15 +0800

    updated vertices loop

--- objects.h
+++ objects.h
@@ -13,30 +13,16 @@ class cube {
     void get_vertices() {
         vect3 val;
 
-        // use bit & operator to identify the 1.. if bit is 1 it is value is negative
+        // use bit & operator to identify the 1.. if a bit is 1 it is value is negative
         for (int i = 0; i < num_vtx; i++) {
-            if ((i & 1) == 1) {
-                val.e[0] = -sides / 2;
-            }
+            for (int j = 0; j < val.size(); j++) {
+                int n = i & (1 << j);
 
-            if ((i & 1) == 0) {
-                val.e[0] = sides / 2;
-            }
-
-            if ((i & 2) == 2) {
-                val.e[1] = -sides / 2;
-            }
-
-            if ((i & 2) == 0) {
-                val.e[1] = sides / 2;
-            }
-
-            if ((i & 4) == 4) {
-                val.e[2] = -sides / 2;
-            }
-
-            if ((i & 4) == 0) {
-                val.e[2] = sides / 2;
+                if (n == (1 << j)) {
+                    val.e[j] = -sides / 2;
+                } else {
+                    val.e[j] = sides / 2;
+                }
             }
 
             vectors.push_back(val);
@@ -63,7 +49,6 @@ class cube {
 
   private:
     const uint32_t num_vtx = 8;
-    const uint32_t num_edg = 12;
     std::vector<vect3> vectors;
     std::vector<std::array<int, 2>> faces;
 };
