6ac612340049f8539129abb39e7ac46868be5ab1
Jummer Ramos <jumjumramos@gmail.com>
2026-01-21 02:41:19 +0800

    added ndebug.h and add validation layers

--- src/triangle.hpp
+++ src/triangle.hpp
@@ -1,10 +1,13 @@
 #ifndef TRIANGLE_HPP
 #define TRIANGLE_HPP
 
+#include "ndebug.h"
 #include "sdl.h"
 #include <SDL_vulkan.h>
+#include <algorithm>
 #include <cstdint>
 #include <cstring>
+#include <stdexcept>
 #include <vector>
 #include <vulkan/vulkan_core.h>
 #include <vulkan/vulkan_raii.hpp>
@@ -25,6 +28,8 @@ class HelloTriangleApplication {
     VkInstance instance;
     VkSurfaceKHR surface;
 
+    vk::raii::Context context;
+
     void initWindow() {
         appWindow.window_width = 1440;
         appWindow.aspect_ratio = 16.0 / 9.0;
@@ -39,6 +44,25 @@ class HelloTriangleApplication {
             "Wireframe Renderer", VK_MAKE_VERSION(1, 0, 0), "Jumz Engine",
             VK_MAKE_VERSION(0, 0, 1), vk::ApiVersion13};
 
+        std::vector<char const *> requiredLayers;
+
+        if (enableValidationLayers)
+            requiredLayers.assign(validationLayers.begin(),
+                                  validationLayers.end());
+
+        auto layerProperties = context.enumerateInstanceLayerProperties();
+
+        if (std::ranges::any_of(requiredLayers, [&layerProperties](
+                                                    auto const &requiredLayer) {
+                return std::ranges::none_of(
+                    layerProperties, [requiredLayer](auto const &layerPropery) {
+                        return strcmp(layerPropery.layerName, requiredLayer) == 0;
+                    });
+            })) {
+            throw std::runtime_error(
+                "One or more required layers are not supported!");
+        }
+
         uint32_t extensionsCount = 0;
 
         SDL_Vulkan_GetInstanceExtensions(appWindow.window, &extensionsCount,
@@ -49,6 +73,9 @@ class HelloTriangleApplication {
 
         VkInstanceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.enabledLayerCount =
+            static_cast<uint32_t>(requiredLayers.size());
+        createInfo.ppEnabledLayerNames = requiredLayers.data();
         createInfo.enabledExtensionCount = extensionsCount;
         createInfo.ppEnabledExtensionNames = extensions.data();
 
@@ -61,6 +88,7 @@ class HelloTriangleApplication {
     }
 
     void mainLoop() {};
+
     void cleanUp() {
         vkDestroySurfaceKHR(instance, surface, nullptr);
         vkDestroyInstance(instance, nullptr);
